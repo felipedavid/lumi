18b503b10089f78b920fb51d1a530c54dc8597f5
Felipe David <felipedavid.huh@gmail.com>
2022-04-02 11:54:01 -0300

    .

--- lumi.c
+++ lumi.c
@@ -1,42 +1,78 @@
 #include <stdio.h>
+#include <string.h>
+#include <sysexits.h>
 
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
 
+Interpret_Result run(const char *source) {
+    printf(source);
+    return INTERPRET_OK;
+}
+
+// NOTE: The source file read by this function is heap allocated
+// and will not be freed. I'm letting the OS take care of that
+// when the process termites. If your PC can't handle all source
+// in main memory, just download more RAM :P
+void run_file(const char *file_name) {
+    // Open file
+    FILE *f = fopen(file_name, "r");
+    if (f == NULL) {
+        fprintf(stderr, "[!] Unable to open file '%s'\n", file_name);
+        exit(EX_IOERR);
+    }
+
+    // Get file size
+    fseek(f, 0L, SEEK_END);
+    size_t file_size = ftell(f);
+    rewind(f);
+
+    // Read file contents
+    char *source = (char *) xmalloc(file_size);
+    fread(source, file_size, 1, f); 
+    fclose(f);
+    source[file_size] = '\0'; // Zero terminates string buffer (overwriting EOF)
+
+    Interpret_Result res = run(source);
+    switch(res) {
+    case INTERPRET_COMPILE_ERROR: exit(EX_DATAERR);
+    case INTERPRET_RUNTIME_ERROR: exit(EX_SOFTWARE);
+    }
+}
+
+void run_prompt() {
+#define INPUT_SIZE 512
+    char input[INPUT_SIZE];
+
+    for (;;) {
+        printf(">>> ");
+
+        if (fgets(input, INPUT_SIZE, stdin) != input) break;
+        input[strlen(input)-1] = '\0'; // Getting rid of newline
+
+        run(input);
+        printf("\n");
+    }
+#undef INPUT_SIZE
+}
+
 int main(int argc, char **argv) {
 #ifdef DEBUG_TESTS
     buf_test();
 #endif
-
-    Chunk chunk;
-
-    chunk_init(&chunk);
-    int constant_index = chunk_push_constant(&chunk, 1.2); 
-    chunk_push_code(&chunk, OP_CONSTANT, 123);
-    chunk_push_code(&chunk, constant_index, 123);
-
-    constant_index = chunk_push_constant(&chunk, 3.4);
-    chunk_push_code(&chunk, OP_CONSTANT, 123);
-    chunk_push_code(&chunk, constant_index, 123);
-
-    chunk_push_code(&chunk, OP_ADD, 123);
-
-    constant_index = chunk_push_constant(&chunk, 5.6);
-    chunk_push_code(&chunk, OP_CONSTANT, 123);
-    chunk_push_code(&chunk, constant_index, 123);
-
-    chunk_push_code(&chunk, OP_DIV, 123);
-    chunk_push_code(&chunk, OP_NEG, 123);
-    chunk_push_code(&chunk, OP_RET, 123);
-
     vm_init();
 
-    vm_interpret(&chunk);
+    if (argc == 1) {
+        run_prompt();
+    } else if (argc == 2) {
+        run_file(argv[1]);
+    } else {
+        fprintf(stderr, "[!] Usage: lumi <script>\n");
+        return EX_USAGE;
+    }
 
     vm_free();
-    chunk_free(&chunk);
-
     return 0;
 }
