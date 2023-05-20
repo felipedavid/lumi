be0ac15fd4656f71ea8af9cccb764bfcb2ad8884
Felipe David <felipedavid.huh@gmail.com>
2023-05-20 18:24:39 -0300

    Adding some operators and starting work on parser

--- main.c
+++ main.c
@@ -121,6 +121,27 @@ typedef enum {
 	TOKEN_FLOAT,
 	TOKEN_STR,
 	TOKEN_NAME,
+  TOKEN_LSHIFT,
+  TOKEN_RSHIFT,
+  TOKEN_EQ,
+  TOKEN_NOTEQ,
+  TOKEN_LTEQ,
+  TOKEN_GTEQ,
+  TOKEN_AND,
+  TOKEN_OR,
+  TOKEN_INC,
+  TOKEN_DEC,
+  TOKEN_COLON_ASSIGN,
+  TOKEN_ADD_ASSIGN,
+  TOKEN_SUB_ASSIGN,
+  TOKEN_OR_ASSIGN,
+  TOKEN_AND_ASSIGN,
+  TOKEN_XOR_ASSIGN,
+  TOKEN_LSHIFT_ASSIGN,
+  TOKEN_RSHIFT_ASSIGN,
+  TOKEN_MUL_ASSIGN,
+  TOKEN_DIV_ASSIGN,
+  TOKEN_MOD_ASSIGN,
 } TokenKind;
 
 typedef enum {
@@ -319,6 +340,28 @@ void scan_str() {
   token.str_val = str;
 }
 
+#define CASE1(c, c1, k1) \
+  case c: \
+    token.kind = *stream++; \
+    if (*stream == c1) { \
+      token.kind = k1; \
+      stream++; \
+    } \
+    break;
+
+#define CASE2(c, c1, k1, c2, k2) \
+  case c: \
+    token.kind = *stream++; \
+    if (*stream == c1) { \
+      token.kind = k1; \
+      stream++; \
+    } else if (*stream == c2) { \
+      token.kind = k2; \
+      stream++; \
+    } \
+    break;
+
+
 // Scans the next token and sets any relevant data in the 'token' global variable
 void next_token() {
 BACK:
@@ -348,11 +391,12 @@ BACK:
     while (isdigit(*stream)) {
       stream++;
     }
-    if (*stream == '.' || tolower(*stream) == 'e') {
-      stream = token.start;
+
+    char ch = *stream;
+    stream = token.start;
+    if (ch == '.' || tolower(ch) == 'e') {
       scan_float();
     } else {
-      stream = token.start;
       scan_int();
     }
 	}	break;
@@ -365,12 +409,54 @@ BACK:
 		token.kind = TOKEN_NAME;
 		token.name = str_intern_range(token.start, stream);
 	} break;
+  case '<':
+    token.kind = *stream;
+    stream++;
+    if (*stream == '<') {
+      token.kind = TOKEN_LSHIFT;
+      stream++;
+      if (*stream == '=') {
+        token.kind = TOKEN_LSHIFT_ASSIGN;
+        stream++;
+      }
+    } else if (*stream == '=') {
+      token.kind = TOKEN_LTEQ;
+      stream++;
+    }
+    break;
+  case '>':
+    token.kind = *stream;
+    stream++;
+    if (*stream == '>') {
+      token.kind = TOKEN_RSHIFT;
+      stream++;
+      if (*stream == '=') {
+        token.kind = TOKEN_RSHIFT_ASSIGN;
+        stream++;
+      }
+    } else if (*stream == '=') {
+      token.kind = TOKEN_GTEQ;
+      stream++;
+    }
+    break;
+  CASE1('^', '=', TOKEN_XOR_ASSIGN); 
+  CASE1(':', '=', TOKEN_COLON_ASSIGN); 
+  CASE1('*', '=', TOKEN_MUL_ASSIGN); 
+  CASE1('/', '=', TOKEN_DIV_ASSIGN); 
+  CASE1('%', '=', TOKEN_MOD_ASSIGN); 
+  CASE2('+', '+', TOKEN_INC, '=', TOKEN_ADD_ASSIGN);
+  CASE2('-', '-', TOKEN_DEC, '=', TOKEN_SUB_ASSIGN);
+  CASE2('&', '&', TOKEN_AND, '=', TOKEN_AND_ASSIGN);
+  CASE2('|', '|', TOKEN_OR, '=', TOKEN_OR_ASSIGN);
 	default:
 		token.kind = *stream++;
 		break;
 	}
 	token.end = stream;
 }
+
+#undef CASE1
+#undef CASE2
 
 // String interning
 typedef struct {
@@ -543,6 +629,19 @@ void lex_test() {
   init_stream("\"foo\" \"a\\nb\"");
   assert_token_str("foo");
   assert_token_str("a\nb");
+  assert_token_eof();
+
+  // Operator tests
+  init_stream(": := + += ++ < <= << <<=");
+  assert_token(':');
+  assert_token(TOKEN_COLON_ASSIGN);
+  assert_token('+');
+  assert_token(TOKEN_ADD_ASSIGN);
+  assert_token(TOKEN_INC);
+  assert_token('<');
+  assert_token(TOKEN_LTEQ);
+  assert_token(TOKEN_LSHIFT);
+  assert_token(TOKEN_LSHIFT_ASSIGN);
   assert_token_eof();
 
   // Misc tests
